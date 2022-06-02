e49178a60584ad1456740e2470d21dc1002b916c
RogalXRogal <53616070+RogalXRogal@users.noreply.github.com>
2022-06-02 12:31:29 +0200

    Add files via upload

---
+++ Sort Python/sort.py
@@ -0,0 +1,69 @@
+import time
+        
+nazwy_plików = []
+zawartość = []
+
+pliczki = [
+    "plik1",
+    "plik2",
+    "plik3",
+    "plik4",
+    "plik5",
+    "plik6",
+    "plik7",
+    "plik8",
+    "plik9",
+    "plik10",
+    "plik11",
+    "plik12",
+    "plik13",
+    "plik14",
+    "plik15",
+    "plik16",
+    "plik17",
+    "plik18",
+    ]
+
+nazwy_plików.append("liczby_losowe_100k.txt")
+nazwy_plików.append("liczby_losowe_10k.txt")
+nazwy_plików.append("liczby_losowe_1k.txt")
+nazwy_plików.append("liczby_malejace_100k.txt")
+nazwy_plików.append("liczby_losowe_10k.txt")
+nazwy_plików.append("liczby_losowe_1k.txt")
+nazwy_plików.append("liczby_malejaco_rosnacy_100k.txt")
+nazwy_plików.append("liczby_malejaco_rosnacy_10k.txt")
+nazwy_plików.append("liczby_malejaco_rosnacy_1k.txt")
+nazwy_plików.append("liczby_rosnace_100k.txt")
+nazwy_plików.append("liczby_rosnace_10k.txt")
+nazwy_plików.append("liczby_rosnace_1k.txt")
+nazwy_plików.append("liczby_rosnaco_malejacy_100k.txt")
+nazwy_plików.append("liczby_rosnaco_malejacy_10k.txt")
+nazwy_plików.append("liczby_rosnaco_malejacy_1k.txt")
+nazwy_plików.append("liczby_stale_100k.txt")
+nazwy_plików.append("liczby_stale_10k.txt")
+nazwy_plików.append("liczby_stale_1k.txt")
+
+
+
+i = 0
+
+for i in range (18):
+
+    pliczki[i] = open(nazwy_plików[i], "r").read()
+
+    lines = pliczki[i].split("\n")
+
+    for line in lines:
+        zawartość.append(int(line))
+
+
+    start = time.time()
+    zawartość.sort()
+    stop = time.time()
+
+    t = stop - start
+
+    print ("Czas wykonania pliku: ")
+    print (nazwy_plików[i])
+    print (t)
+
